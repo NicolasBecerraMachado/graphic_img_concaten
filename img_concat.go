5249ce32e3b06097b2eb299f8cda41604290551c
Nicolás Roberto Becerra Machado <NicolasBecMa1999@outlook.com>
2023-06-07 18:51:12 +0000

    add program prototype
    Signed-off-by: Nicolás Roberto Becerra Machado <NicolasBecMa1999@outlook.com>

---
+++ img_concat.go
@@ -0,0 +1,30 @@
+//joins images the image in the second parameter to the bottom in the first
+//images should have the sema bAse length
+//returns joint image
+func UneY(){
+
+}
+
+//joins images the image in the second parameter to the image in the first
+//images should have the same height length
+//returns joint image
+func UneX(){
+
+}
+
+//rotates image 90°
+//returns rotated image
+func GIRO(){
+
+}
+
+func main(){
+	//read images
+
+	//call functions to make transformations
+
+	//print image
+
+	//store image
+	
+}
