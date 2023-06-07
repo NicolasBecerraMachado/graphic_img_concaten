d62089135b026a2ddba72e7eb0f82f7a43226d7a
Nicolás Roberto Becerra Machado <NicolasBecMa1999@outlook.com>
2023-06-07 22:52:54 +0000

    properly read and getting images dimensions

--- img_concat.go
+++ img_concat.go
@@ -5,7 +5,8 @@ import(
 	"os"
 	"log"
 	"image"
-
+	"bufio"
+	//"io"
 	_ "image/png"
 )
 
@@ -26,14 +27,17 @@ func UneX(){
 
 //rotates image 90°
 //returns rotated image
-func GIRO(){
+func GIRO(im *image.NRGBA){
+	/*for i := 0; i < im.Height; i++{
+		for j := 0; j < im.Width; j++{
 
+		}
+	}*/
 }
 
 //load image
-func load(filePath string) *image.Gray {
+func load(filePath string) (*image.NRGBA) {
 	imgFile, err := os.Open(filePath)
-	defer imgFile.Close()
 	if err != nil {
 		log.Println("Cannot read file:", err)
 	}
@@ -42,15 +46,42 @@ func load(filePath string) *image.Gray {
 	if err != nil {
 		log.Println("Cannot decode file:", err)
 	}
-	return img.(*image.Gray)
+
+	return img.(*image.NRGBA)
+}
+
+func getConf(filePath string) image.Config{
+	imgFile, err := os.Open(filePath)
+	if err != nil {
+		log.Println("Cannot read file:", err)
+	}
+	
+	reader := bufio.NewReader(imgFile)
+
+	Rconfig,_,err := image.DecodeConfig(reader)
+	if err != nil {
+		log.Println("Cannot get config:", err)
+	}
+
+	return Rconfig
 }
 
 func main(){
 	//read images
 	R := load("R.png")
-	S := load("S.png")
+	
+	S:= load("S.png")
+
+	Rconfig := getConf("R.png")
+
+	Sconfig := getConf("S.png")
+
+	fmt.Println("--------------------------")
 	fmt.Println(R.Pix[0])
-	fmt.Println(S.Pix[0])
+	fmt.Println(S.Pix[0], S.Pix[1], S.Pix[255*255])
+	fmt.Println("--------------------------")
+	fmt.Println(Rconfig.Height)
+	fmt.Println(Sconfig.Width)
 	//call functions to make transformations
 
 	//print image
