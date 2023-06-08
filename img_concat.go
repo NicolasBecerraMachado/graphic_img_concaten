5b8247e793074c0af12145d3503c94954e7e8fa0
Nicolás Roberto Becerra Machado <NicolasBecMa1999@outlook.com>
2023-06-08 03:38:50 +0000

    able to turn images, config dependency eliminated
    Signed-off-by: Nicolás Roberto Becerra Machado <NicolasBecMa1999@outlook.com>

--- img_concat.go
+++ img_concat.go
@@ -6,8 +6,10 @@ import(
 	"log"
 	"image"
 	"bufio"
+	//"crypto/rand"
 	//"io"
-	_ "image/png"
+	"image/png"
+	//"string"
 )
 
 
@@ -21,18 +23,31 @@ func UneY(){
 //joins images the image in the second parameter to the image in the first
 //images should have the same height length
 //returns joint image
-func UneX(){
+func UneX(im1, im2 *image.NRGBA){
 
 }
 
-//rotates image 90°
+//rotates image 180°
 //returns rotated image
-func GIRO(im *image.NRGBA){
-	/*for i := 0; i < im.Height; i++{
-		for j := 0; j < im.Width; j++{
+func GIRO(im *image.NRGBA) (*image.NRGBA){
+	//fmt.Println("size ni = ",conf.Height, conf.Height)
+	
+	pix := make([]uint8, im.Rect.Max.X*im.Rect.Max.Y*4)
 
+	for i := 0; i < im.Rect.Max.Y; i++{
+		for j := 0; j < im.Rect.Max.Y; j++{
+			for k:= 0; k < 4; k++{
+				pix[j*im.Rect.Max.X*4 + i*4 + k] = im.Pix[i*im.Rect.Max.X*4 + j*4 + k]
+			}
 		}
-	}*/
+	}
+	//rand.Read(pix)
+	created := &image.NRGBA{
+		Pix:    pix,
+		Stride: im.Rect.Max.X*2 + im.Rect.Max.Y*2,
+		Rect:   image.Rect(0,0,im.Rect.Max.Y, im.Rect.Max.X),
+	}
+	return created
 }
 
 //load image
@@ -50,20 +65,14 @@ func load(filePath string) (*image.NRGBA) {
 	return img.(*image.NRGBA)
 }
 
-func getConf(filePath string) image.Config{
-	imgFile, err := os.Open(filePath)
+func save(filePath string, img *image.NRGBA) {
+	imgFile, err := os.Create(filePath)
+	defer imgFile.Close()
 	if err != nil {
-		log.Println("Cannot read file:", err)
+		log.Println("Cannot create file:", err)
 	}
-	
-	reader := bufio.NewReader(imgFile)
-
-	Rconfig,_,err := image.DecodeConfig(reader)
-	if err != nil {
-		log.Println("Cannot get config:", err)
-	}
-
-	return Rconfig
+	fmt.Println("weird line")
+	png.Encode(imgFile, img.SubImage(img.Rect))
 }
 
 func main(){
@@ -72,18 +81,30 @@ func main(){
 	
 	S:= load("S.png")
 
-	Rconfig := getConf("R.png")
-
-	Sconfig := getConf("S.png")
-
 	fmt.Println("--------------------------")
 	fmt.Println(R.Pix[0])
 	fmt.Println(S.Pix[0], S.Pix[1], S.Pix[255*255])
 	fmt.Println("--------------------------")
-	fmt.Println(Rconfig.Height)
-	fmt.Println(Sconfig.Width)
 	//call functions to make transformations
+	tst := GIRO(S)
+	
+	fmt.Println(len(S.Pix))
 
+	/*f, err := os.Create("data.txt")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer f.Close()
+
+	_, err2 := f.WriteString(string(tst.Pix))
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }*/
+
+	save("tst.png", tst)
 	//print image
 
 	//store image
